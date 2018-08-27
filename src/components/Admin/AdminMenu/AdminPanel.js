3bd55703a782025b7cbd4d405d91dd10580e2e62
diurivj <diurivj@icloud.com>
2018-08-27 11:05:57 -0500

    brendi changes

--- src/components/Admin/AdminMenu/AdminPanel.js
+++ src/components/Admin/AdminMenu/AdminPanel.js
@@ -14,66 +14,62 @@ class AdminPanel extends Component{
   render(){
     return(
         <div style={{ width: 200 , height:'100vh', textAlign: 'left' }}>
-          <Menu mode="inline" theme="dark" inlineCollapsed={false} defaultSelectedKeys={['1']} style={{ height: '100vh' }} inlineIndent={'24'} >
-            <div style={{ marginTop: '30px', marginBottom: '30px', textAlign: 'center' }}>
-              <img src={logo} alt="allende" width="70%" />
-            </div>
+          <Menu mode="inline" theme="dark" inlineCollapsed={false} defaultSelectedKeys={['1']} style={{ height: '100vh' }} >
+
+            <Menu.Item key="/admin" style={{height: '100px', textAlign: "center", marginTop: '50px', marginBottom: '50px'}} >
+              <Link to="/admin">
+                <img src={logo} alt="allendelogo" width="70%"/>
+              </Link>
+            </Menu.Item>
+
             <Menu.Item key="1">
               <Link to="/admin">
-                <div className='menuItem'>
-                  <Icon type="dashboard" />
-                  <span>Dashboard</span>
-                </div>
+                <Icon type="dashboard" />
+                <span>Dashboard</span>
               </Link>
             </Menu.Item>
+
             <Menu.Item  key="2">
               <Link to="/admin/dist">
-                <div className='menuItem'>
-                  <Icon type="team" />
-                  <span>Distribuidores</span>
-                </div>
+                <Icon type="team" />
+                <span>Distribuidores</span>
               </Link>
             </Menu.Item>
+
             <Menu.Item  key="3">
               <Link to="/admin/pedidos">
-                <div className='menuItem'>
-                  <Icon type="inbox" />
-                  <span>Pedidos</span>
-                </div>
+                <Icon type="inbox" />
+                <span>Pedidos</span>
               </Link>
             </Menu.Item>
+
             <SubMenu key="sub1" title={<span><Icon type="appstore" /><span>Productos</span></span>}>
               <Menu.Item key="4">
                 <Link to="/admin/products">
-                  <div className='menuItem'>
-                    <span>Todos los Productos</span>
-                  </div>
+                  <span>Todos los Productos</span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="5">
                 <Link to="/admin/promos" >
-                  <div className='menuItem'>
-                    <span>Promociones</span>
-                  </div>
+                  <span>Promociones</span>
                 </Link>
               </Menu.Item>
             </SubMenu>
+
             <Menu.Item  key="6">
               <Link to="/admin/profile">
-                <div className='menuItem'>
-                  <Icon type="user" />
-                  <span>Perfil</span>
-                </div>
+                <Icon type="user" />
+                <span>Perfil</span>
               </Link>
             </Menu.Item>
+
             <Menu.Item onClick={this.logOut} key="7">
               <Link to="/">
-                <div className='menuItem'>
-                  <Icon type="poweroff" />
-                  <span>Logout</span>
-                </div>
+                <Icon type="poweroff" />
+                <span>Logout</span>
               </Link>
             </Menu.Item>
+
           </Menu>
       </div>
     )
