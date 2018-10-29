86b7557a2afa0dc77ece854425421bad7a68a8f6
hectorbliss <contacto@fixter.org>
2018-10-28 21:34:47 -0600

    form

--- src/components/Distribuidor/forms/DistribuidorNewPedido.js
+++ src/components/Distribuidor/forms/DistribuidorNewPedido.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../Admin.css';
-import { Form, Input, Select, InputNumber, DatePicker } from 'antd';
+import { Form, Input, Select, InputNumber, Button, Icon} from 'antd';
+import FontAwesome from "react-fontawesome"
 
 const FormItem = Form.Item;
 const { TextArea } = Input;
@@ -25,19 +26,24 @@ export const DistribuidorNewPedido  = () => {
                 {/* <FormItem label="Fecha de entrega requerida">
                     <DatePicker  onChange={onChange} />
                 </FormItem> */}
-
+                <p>Genera tu pedido</p>
                 <div className="flexito">
-                    <FormItem label="IPA" style={{ width:"60%" }}>
-                        <InputNumber  min={1} max={100} defaultValue={10} onChange={onChange} />
+                    
+                    <FormItem label="Producto">
+                        <Select defaultValue="lucy" style={{ width: 120 }} onChange={handleChange}>
+                            <Option value="jack">Jack</Option>
+                            <Option value="lucy">Lucy</Option>
+                            <Option value="Yiminghe">yiminghe</Option>
+                        </Select>
                     </FormItem>
-                    <FormItem label="IPA" style={{ width:"60%" }}>
-                        <InputNumber min={1} max={100} defaultValue={10} onChange={onChange} />
+                    <FormItem label="Caja(24pzas)">
+                       <InputNumber  min={1} max={100} defaultValue={10} onChange={onChange} />
                     </FormItem>
-                    <FormItem label="IPA" style={{ width:"60%" }}>
-                        <InputNumber min={1} max={100} defaultValue={10} onChange={onChange} />
+                    <FormItem label="Precio u">
+                       <InputNumber  defaultValue={10}  />
                     </FormItem>
-                    <FormItem label="IPA" style={{ width:"60%" }}>
-                        <InputNumber min={1} max={100} defaultValue={10} onChange={onChange} />
+                    <FormItem label=" ">
+                       <Button type="primary">Agregar</Button>
                     </FormItem>
                 </div>
                 <FormItem >Total: $400.00</FormItem>
